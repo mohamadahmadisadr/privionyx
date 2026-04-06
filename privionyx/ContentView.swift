0fc2c3fcfd5583a63d74ddea3e38dfdd4b0e13ca
mohamadahmadisadr <mohamadahmadisadr@gmail.com>
2026-04-05 23:53:13 -0400

    Initial Commit

---
+++ privionyx/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  privionyx
+//
+//  Created by Mohammad Ahmadi on 2026-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ privionyx/privionyxApp.swift
@@ -0,0 +1,17 @@
+//
+//  privionyxApp.swift
+//  privionyx
+//
+//  Created by Mohammad Ahmadi on 2026-04-05.
+//
+
+import SwiftUI
+
+@main
+struct privionyxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
